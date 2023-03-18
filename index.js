8101f9fca25195b1b2cda791d75e4c1867d9a2a3
kevin Wairi <kevinwairi@gmail.com>
2023-03-18 21:32:36 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2= 2;
+let num3=25;
+let num4= 7;
 
-// Write your code here
+let multiply = num1*num2;
+console.log(multiply); 
+
+
+const random = Math.floor(Math.random() * 10)+ 1;
+console.log(random); 
+
+let mod =num3%num4;
+console.log(mod); 
+
+let max = Math.max(10,15,17,20);
+console.log(max); 
